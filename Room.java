d3078a2c927b4bd7f6d3f41625eeb9b18bf07bf1
Yuan-Chang <k21asdd@hotmail.com>
2015-08-27 17:08:07 +0800

    use static method to control Room

--- Room.java
+++ Room.java
@@ -1,8 +1,38 @@
 package Server;
 
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Stack;
 
-public class Room {
-	private Socket[] guests;
-	private String[] info;
+public final class Room {
+	private static ArrayList<Socket> SocketArr = new ArrayList<Socket>();
+	private static ArrayList<String> rooms =  new ArrayList<String>();
+	private static Stack<Integer> mStack = new Stack<Integer>();
+	
+	private Room(){}
+	public synchronized static int addRoom(String s, Socket guest){
+		int index;
+		if(mStack.isEmpty()){
+			index = rooms.size();
+			rooms.add(s + " " + rooms.size());
+			SocketArr.add(guest);
+		}
+		else{
+			index = mStack.pop();
+			rooms.add(index, s + " " + index);
+			SocketArr.add(index, guest);
+		}
+		return index;
+	}
+	public synchronized static void removeRoom(int index){
+		rooms.set(index, null);
+		SocketArr.set(index, null);
+		mStack.push(Integer.valueOf(index));
+	}
+	public synchronized static Socket getRoomMaster(int index){
+		return SocketArr.get(index);
+	}
+	public synchronized static String[] getRooms(){
+		return rooms.toArray(new String[rooms.size()]);
+	}
 }
