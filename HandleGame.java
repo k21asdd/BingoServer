3bb7469793c1fffb8b3663dd5d67eb33ef799098
Yuan-Chang <k21asdd@hotmail.com>
2015-08-27 17:07:09 +0800

    Add Q_CONNECT

--- HandleGame.java
+++ HandleGame.java
@@ -10,10 +10,14 @@ public class HandleGame extends Thread{
 	private Socket guest;
 	private Socket master;
 	private BufferedReader in;
+	private PrintWriter out;
 	public HandleGame(Socket guest, Socket master) throws IOException{
 		this.guest = guest;
 		this.master = master;
 		in = new BufferedReader(new InputStreamReader(master.getInputStream()));
+		out = new PrintWriter(guest.getOutputStream());
+		out.println("Q_CONNECT");
+		out.flush();
 	}
 	@Override
 	public void run() {
@@ -30,17 +34,21 @@ public class HandleGame extends Thread{
 			}
 		}
 		try {
-			new PrintWriter(master.getOutputStream(),true).println("AiaiBianOK");
+			out.println("Q_DISCONNECT");
+			out.flush();
 			System.out.println(master.getPort()+" : close");
 			master.close();
 			master = null;
+			in.close();
+			in = null;
+			out.close();
+			out = null;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	private void handleMessage(int signal, String data) throws IOException{
-		PrintWriter out= new PrintWriter(guest.getOutputStream());
 		out.println(signal);
 		out.println(data);
 		out.flush();
