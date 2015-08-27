1de56cba5de06d48e83b9406e2b3f92ebc5ebcb4
Yuan-Chang <k21asdd@hotmail.com>
2015-08-27 15:37:24 +0800

    Control signal

---
+++ BingoSignal.java
@@ -0,0 +1,10 @@
+package Server;
+
+public class BingoSignal {
+	public static final int QUERY = 0;
+	public static final int CREATE = 1;
+	public static final int CONNECT = 2;
+	public static final int STEP = 3;
+	public static final int CLOSE = 4;
+	public static final int CHAT = 5;
+}
