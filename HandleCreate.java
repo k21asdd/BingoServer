bed790c30223adc60aa4763d8675fcccbdce901a
Yuan-Chang <k21asdd@hotmail.com>
2015-09-24 17:23:16 +0800

    If receive null data, means client close socket

--- HandleCreate.java
+++ HandleCreate.java
@@ -18,22 +18,30 @@ public class HandleCreate extends Thread{
 		// TODO Auto-generated method stub
 		Socket nClient;
 		try {
-			ServerSocket subServer = new ServerSocket(0);
 			PrintWriter out = new PrintWriter(oClient.getOutputStream());
-			System.out.println("New port ?" + subServer.getLocalPort());
 			//read data from old socket port
 			String data = new BufferedReader(new InputStreamReader(oClient.getInputStream())).readLine();
 			System.out.println(data);
+			if(data == null){
+				System.out.println("Creation Abort");
+				return;
+			}
+			//Create New port for specific client
+			ServerSocket subServer = new ServerSocket(0);
+			System.out.println("New port ?" + subServer.getLocalPort());
 			out.println(subServer.getLocalPort());
 			out.flush();
+			//wait for client
 			nClient = subServer.accept();
 			System.out.println("Accept");
+			oClient.close();
+			//get output stream of new port
 			out = new PrintWriter(nClient.getOutputStream()); 
-			out.println(BingoSignal.CREATE);
 			int index = Room.addRoom(data, new Pair(nClient, subServer));
 			out.println("Q_OK");
 			out.println(index);
 			out.flush();
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
